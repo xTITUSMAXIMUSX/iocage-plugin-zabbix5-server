3e0446617e58f87db2d86579a02b03b83c0818c2
xTITUSMAXIMUSX <62310795+xTITUSMAXIMUSX@users.noreply.github.com>
2021-03-27 04:20:50 -0500

    Update zabbix.conf.php

--- zabbix.conf.php
+++ zabbix.conf.php
@@ -5,7 +5,7 @@ $DB['TYPE']                             = 'MYSQL';
 $DB['SERVER']                   = 'localhost';
 $DB['PORT']                             = '0';
 $DB['DATABASE']                 = 'zabbix';
-$DB['USER']                             = 'zabbix';
+$DB['USER']                             = 'root';
 $DB['PASSWORD'] = '';
 
 // Schema name. Used for PostgreSQL.
